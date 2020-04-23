feffc100c335cd71758caab8c559180f6fad6bb8
raxanob <rayanexavier161@gmail.com>
2020-04-23 16:05:47 -0300

    Exercicío 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Exercício 1 - Escreva uma closure que imprime "Quarentena Recursiva" e atribui a closure a uma variável. Em seguida chame a closure
+
+var clousure = {
+    print ("Quarentena Recursiva")
+}
+clousure()
+
+//
